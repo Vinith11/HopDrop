e8c94182a479fd0244e4f4445f675c3656ed2e04
Vinith11 <vinith2003@gmail.com>
2025-01-07 13:46:38 +0530

    Add user registration functionality with validation and JWT authentication

--- Backend/app.js
+++ Backend/app.js
@@ -1,18 +1,25 @@
 const dotenv = require('dotenv');
-dotenv.config();
-
 const express = require('express');
 const app = express();
-
 const connectDB = require('./db/db');
+const cors = require('cors');
+const userRoutes = require('./routes/user.routes');
 
+
+dotenv.config();
 connectDB();
 
-const cors = require('cors');
+//config
 app.use(cors()); // Allow Cross-Origin Resource Sharing
+app.use(express.json()); // Parse incoming request body in JSON format
+app.use(express.urlencoded({extended: true})); // Parse incoming request body in URL encoded format
+
 
 app.get('/', (req, res)=>{
     res.send('Hello World');
 })
+
+app.use('/user', userRoutes);
+
 
 module.exports = app;
--- Backend/models/user.model.js
+++ Backend/models/user.model.js
@@ -1,29 +1,50 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+const bcrtpt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 const userSchema = new mongoose.Schema({
-    fullname:{
-        firstname:{
-            type: String,
-            required: true,
-            minlength: [3, 'First name must be at least 3 characters long'],
-        },
-        lastname:{
-            type: String,
-            minlength: [3, 'Last name must be at least 3 characters long'],
-        }
+  fullname: {
+    firstname: {
+      type: String,
+      required: true,
+      minlength: [3, "First name must be at least 3 characters long"],
     },
-    email:{
-        type: String,
-        required: true,
-        unique: true,
-        minlength: [5, 'Email must be at least 5 characters long'],
+    lastname: {
+      type: String,
+      minlength: [3, "Last name must be at least 3 characters long"],
     },
-    password:{
-        type: String,
-        required: true
-    },
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    minlength: [5, "Email must be at least 5 characters long"],
+  },
+  password: {
+    type: String,
+    required: true,
+    select: false,
+  },
 
-    socketId: {
-        type: String,
-    },
+  socketId: {
+    type: String,
+  },
 });
+
+userSchema.methods.generateAuthToken = function () {
+  const token = jwt.sign({ _id: this._id }, process.env.JWT_SECRET);
+
+  return token;
+};
+
+userSchema.methods.comparePassword = async function (enteredPassword) {
+  return await bcrtpt.compare(enteredPassword, this.password);
+};
+
+userSchema.statics.hashPassword = async function (password) {
+  return await bcrtpt.hash(password, 10);
+};
+
+const userModel = mongoose.model("User", userSchema);
+
+module.exports = userModel;
