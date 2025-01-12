10aa3967d475ff8ec3ddd2b3086e31b8268e8c63
Vinith11 <vinith2003@gmail.com>
2025-01-12 20:24:30 +0530

    Alpha version completed

--- Frontend/src/pages/Start.jsx
+++ Frontend/src/pages/Start.jsx
@@ -1,24 +1,25 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import logo from "../assets/2.png";
 
 const Start = () => {
   return (
-    <div>
-      <div className="bg-cover bg-center bg-[url(https://images.unsplash.com/photo-1692910410341-cf21779ebbe4?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTZ8fHRyYWZmaWMlMjBsaWdodHxlbnwwfHwwfHx8MA%3D%3D)] h-screen pt-8 flex justify-between w-full flex-col bg-red-400">
+    <div className="bg-black h-screen flex flex-col justify-between">
+      <div className="flex-1 flex items-center justify-center">
         <img
-          className="w-16 ml-8"
-          src="https://upload.wikimedia.org/wikipedia/commons/c/cc/Uber_logo_2018.png"
-          alt=""
+          className="w-66"
+          src={logo}
+          alt="HopDrop Logo"
         />
-        <div className="bg-white pb-7 py-4 px-4">
-          <h2 className="text-3xl font-bold">Get Started with Uber</h2>
-          <Link
-            to="/user/signin"
-            className="flex items-center justify-center w-full bg-black text-white py-3 rounded mt-5"
-          >
-            Contine
-          </Link>
-        </div>
+      </div>
+      <div className="bg-white pb-7 py-4 px-4">
+        <h2 className="text-2xl text-center font-bold">Get Started with HopDrop</h2>
+        <Link
+          to="/user/signin"
+          className="flex items-center justify-center w-full bg-black text-white py-3 rounded mt-5"
+        >
+          Continue
+        </Link>
       </div>
     </div>
   );
